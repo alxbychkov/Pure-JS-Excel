f5596468c633e483ce42f4179c6b36ad96171e20
Work <Work@Alexanders-MacBook-Pro.local>
2020-08-29 17:16:40 +0300

    start new branch logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import { ExcelComponent } from '@core/ExcelComponent'
 import { createTable } from './table.template'
 import { resizeHandler } from './table.resize'
 import { shouldResize } from '@/components/table/table.functions'
+import { TableSelection } from './TableSelection'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -13,6 +14,14 @@ export class Table extends ExcelComponent {
   }
   toHTML() {
     return createTable(20)
+  }
+  prepare() {
+    this.selection = new TableSelection()
+  }
+  init() {
+    super.init()
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selection.select($cell)
   }
   onClick() { }
   onMousedown(event) {
