69948a78bd27590dbae4902d964206d0e7dfde18
Alexander Bychkov <51967476+alxbychkov@users.noreply.github.com>
2020-09-11 10:57:13 +0300

    Table logic (#9)
    * start new branch logic
    
    * start to meke Emitter
    
    * Finish table logic
    
    Co-authored-by: Work <Work@Alexanders-MacBook-Pro.local>

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,14 +4,33 @@ export class ExcelComponent extends DomListener {
     constructor($root, options = {}) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers = []
+        this.prepare()
     }
+    // Настраиваем наш компонент до init
+    prepare() {}
+    // Возвращает шаблон компонента
     toHTML() {
         return ''
     }
+    // Уведомляем слушателей про события event
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+    // Подписываемся на события event
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
+    }
+
+    // Инициализируем компонент, добавляем DOM слушателей
     init() {
         this.initDOMListeners()
     }
+    // Удаляем компонент, чистим слушателей
     destroy() {
         this.removeDOMListeners()
+        this.unsubscribers.forEach(unsub=>unsub())
     }
 }
